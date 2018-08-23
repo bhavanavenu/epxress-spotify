6a7a0d5b9aaff8633113bfc32c2203af957ba900
Bhavana <bhavanavenugopal2@gmail.com>
2018-08-23 23:58:19 +0200

    added app.js

--- starter_code_cypress/app.js
+++ starter_code_cypress/app.js
@@ -3,6 +3,25 @@ const hbs = require('hbs');
 
 const app = express();
 
+const SpotifyWebApi = require('spotify-web-api-node');
+
+// Remember to paste here your credentials
+const clientId = '1c30624cba6742dcb792991caecae571', // TO CHANGE
+    clientSecret = '746977b1e77240faa9d0d2411c3e0efe'; // TO CHANGE 
+
+const spotifyApi = new SpotifyWebApi({
+  clientId : clientId,
+  clientSecret : clientSecret
+});
+
+// Retrieve an access token.
+spotifyApi.clientCredentialsGrant()
+  .then(function(data) {
+    spotifyApi.setAccessToken(data.body['access_token']);
+  }, function(err) {
+    console.log('Something went wrong when retrieving an access token', err);
+});
+
 app.set('views', __dirname + '/views');
 app.set('view engine', 'hbs');
 app.use(express.static('public'));
